524e6027f3f09a26f356ef79196c05f8565530fe
Kasumov Artur <artur.ka@nextcode.tech>
2026-02-18 17:51:52 +0200

    entering the security zone 🔐

---
+++ section1/src/main/java/com/arturk/Section1Application.java
@@ -0,0 +1,12 @@
+package com.arturk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Section1Application.class, args);
+    }
+}
---
+++ section1/src/main/java/com/arturk/controller/WelcomeController.java
@@ -0,0 +1,14 @@
+package com.arturk.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping(value = "/welcome")
+    public String welcome() {
+        return "Welcome to Spring Security Udemy Course";
+    }
+
+}
