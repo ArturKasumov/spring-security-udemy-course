a5cdca38259ad168680646e531f023d181a0fe37
Kasumov Artur <artur.ka@nextcode.tech>
2026-02-20 16:45:05 +0200

    practice Spring Security setup 🛡️

---
+++ section2/src/main/java/com/arturk/Section2Application.java
@@ -0,0 +1,12 @@
+package com.arturk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Section2Application.class, args);
+    }
+}
