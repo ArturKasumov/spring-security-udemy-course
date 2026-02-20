9e2066b0cac869c5bb7f19004049f0ef545d2389
Kasumov Artur <artur.ka@nextcode.tech>
2026-02-20 18:15:06 +0200

    practice Spring Security setup 🛡️

---
+++ section3/src/main/java/com/arturk/Section3Application.java
@@ -0,0 +1,12 @@
+package com.arturk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Section3Application.class, args);
+    }
+}
---
+++ section3/src/main/java/com/arturk/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.arturk.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) ->
+                requests.requestMatchers("/notices", "/contact").permitAll()
+                        .anyRequest().authenticated()
+        );
+        http.formLogin(withDefaults());
+        http.httpBasic(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        UserDetails user = User.builder().username("user").password("$2a$12$ft.yzbx49qqRvk5pCzAnT.yEMIT5mzV1VcDOvcVUAz.bTvbFeMveW").authorities("user").build();
+        UserDetails admin = User.builder().username("admin").password("$2a$12$ft.yzbx49qqRvk5pCzAnT.yEMIT5mzV1VcDOvcVUAz.bTvbFeMveW").authorities("admin").build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
