82407582e27b954ca42c598276a7d169981dd9dd
Kasumov Artur <artur.ka@nextcode.tech>
2026-02-28 10:47:50 +0200

    🕵️‍♂️ Made security less trusting and more suspicious

--- section7/src/main/java/com/arturk/controller/CustomerController.java
+++ section7/src/main/java/com/arturk/controller/CustomerController.java
@@ -2,11 +2,15 @@ package com.arturk.controller;
 
 import com.arturk.model.entity.CustomerEntity;
 import com.arturk.service.CustomerService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.SneakyThrows;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/customer")
@@ -18,5 +22,16 @@ public class CustomerController {
     @PostMapping(value = "/register")
     public CustomerEntity createCustomer(@RequestBody CustomerEntity customer) {
         return customerService.createCustomer(customer);
+    }
+
+    @SneakyThrows
+    @GetMapping
+    public String getCustomer() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Map<String, String> map = new HashMap<>();
+        map.put("username", authentication.getName());
+        map.put("password", authentication.getCredentials() != null ? authentication.getCredentials().toString() : "");
+        map.put("role", authentication.getAuthorities() != null ? authentication.getAuthorities().toString() : "");
+        return new ObjectMapper().writeValueAsString(map);
     }
 }
