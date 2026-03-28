402c640c8ce65b2679c7e12c564cab44861c585d
Kasumov Artur <artur.ka@nextcode.tech>
2026-03-28 17:42:22 +0200

    Another step into spring security filters 🔍🔐

--- section10/src/main/java/com/arturk/controller/CustomerController.java
+++ section10/src/main/java/com/arturk/controller/CustomerController.java
@@ -2,15 +2,8 @@ package com.arturk.controller;
 
 import com.arturk.model.entity.CustomerEntity;
 import com.arturk.service.CustomerService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/customer")
@@ -29,14 +22,8 @@ public class CustomerController {
         return customerService.getCustomerByEmail(email);
     }
 
-    @SneakyThrows
     @GetMapping
     public String getCurrentCustomer() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        Map<String, String> map = new HashMap<>();
-        map.put("username", authentication.getName());
-        map.put("password", authentication.getCredentials() != null ? authentication.getCredentials().toString() : "");
-        map.put("role", authentication.getAuthorities() != null ? authentication.getAuthorities().toString() : "");
-        return new ObjectMapper().writeValueAsString(map);
+        return customerService.getCurrentCustomer();
     }
 }
--- section10/src/main/java/com/arturk/service/CustomerService.java
+++ section10/src/main/java/com/arturk/service/CustomerService.java
@@ -2,10 +2,17 @@ package com.arturk.service;
 
 import com.arturk.model.entity.CustomerEntity;
 import com.arturk.repository.CustomerRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -27,5 +34,15 @@ public class CustomerService {
         }
         customer.setPassword(passwordEncoder.encode(customer.getPassword()));
         return customerRepository.save(customer);
+    }
+
+    @SneakyThrows
+    public String getCurrentCustomer() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Map<String, String> map = new HashMap<>();
+        map.put("username", authentication.getName());
+        map.put("password", authentication.getCredentials() != null ? authentication.getCredentials().toString() : "");
+        map.put("role", authentication.getAuthorities() != null ? authentication.getAuthorities().toString() : "");
+        return new ObjectMapper().writeValueAsString(map);
     }
 }
