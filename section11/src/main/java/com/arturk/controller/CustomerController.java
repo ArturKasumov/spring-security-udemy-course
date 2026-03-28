6b9cadfca74810f7bb19656afa9ddc358308d4ca
Kasumov Artur <artur.ka@nextcode.tech>
2026-03-28 17:46:27 +0200

    JWT magic happens here 🔐🪄

--- section11/src/main/java/com/arturk/controller/CustomerController.java
+++ section11/src/main/java/com/arturk/controller/CustomerController.java
@@ -3,18 +3,8 @@ package com.arturk.controller;
 import com.arturk.model.dto.LoginRequest;
 import com.arturk.model.entity.CustomerEntity;
 import com.arturk.service.CustomerService;
-import com.arturk.utils.JwtUtil;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/customer")
@@ -22,8 +12,6 @@ import java.util.Map;
 public class CustomerController {
 
     private final CustomerService customerService;
-    private final JwtUtil jwtUtil;
-    private final AuthenticationManager authenticationManager;
 
     @PostMapping(value = "/register")
     public CustomerEntity createCustomer(@RequestBody CustomerEntity customer) {
@@ -36,24 +24,12 @@ public class CustomerController {
     }
 
     @PostMapping("/login")
-    public String getJwtToken(@RequestBody LoginRequest loginRequest) {
-        Authentication authenticated = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        loginRequest.getEmail(), loginRequest.getPassword()
-                )
-        );
-
-        return jwtUtil.generateToken(authenticated);
+    public String login(@RequestBody LoginRequest loginRequest) {
+        return customerService.login(loginRequest);
     }
 
-    @SneakyThrows
     @GetMapping
     public String getCurrentCustomer() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        Map<String, String> map = new HashMap<>();
-        map.put("username", authentication.getName());
-        map.put("password", authentication.getCredentials() != null ? authentication.getCredentials().toString() : "");
-        map.put("role", authentication.getAuthorities() != null ? authentication.getAuthorities().toString() : "");
-        return new ObjectMapper().writeValueAsString(map);
+        return customerService.getCurrentCustomer();
     }
 }
--- section11/src/main/java/com/arturk/service/CustomerService.java
+++ section11/src/main/java/com/arturk/service/CustomerService.java
@@ -1,11 +1,22 @@
 package com.arturk.service;
 
+import com.arturk.model.dto.LoginRequest;
 import com.arturk.model.entity.CustomerEntity;
 import com.arturk.repository.CustomerRepository;
+import com.arturk.utils.JwtUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -13,6 +24,8 @@ public class CustomerService {
 
     private final CustomerRepository customerRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+    private final AuthenticationManager authenticationManager;
 
     public CustomerEntity getCustomerByEmail(String email){
         return customerRepository.findByEmail(email)
@@ -27,5 +40,24 @@ public class CustomerService {
         }
         customer.setPassword(passwordEncoder.encode(customer.getPassword()));
         return customerRepository.save(customer);
+    }
+
+    @SneakyThrows
+    public String getCurrentCustomer() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Map<String, String> map = new HashMap<>();
+        map.put("username", authentication.getName());
+        map.put("password", authentication.getCredentials() != null ? authentication.getCredentials().toString() : "");
+        map.put("role", authentication.getAuthorities() != null ? authentication.getAuthorities().toString() : "");
+        return new ObjectMapper().writeValueAsString(map);
+    }
+
+    public String login(LoginRequest loginRequest) {
+        Authentication authenticated = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getEmail(), loginRequest.getPassword()
+                )
+        );
+        return jwtUtil.generateToken(authenticated);
     }
 }
